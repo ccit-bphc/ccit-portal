ee2bf6d25b9ba2befe537a07741b523052d69aed
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-08-17 00:16:20 +0530

    Complaint Register View Updated

--- complaints/views.py
+++ complaints/views.py
@@ -17,16 +17,19 @@ def previous(request):
 
 
 def complaint_register(request):
-    if not request.user.is_authenticated:
-        return render(request, "registration/home.html", context={"title": "home"})
+    #if not request.user.is_authenticated:
+     #   return render(request, "registration/home.html", context={"title": "home"})
 
-    form = Complaint_form(request.POST, request.FILES)
+    form = Complaint_form(request.POST)
     if form.is_valid():
         form_obj = form.save(commit=False)
         form_obj.user = request.user
         form_obj.save()
         message.success(request, "Your Complaint has been Successfully Registered")
         return render(request, "registration/home.html", context={"title": "home"})
+    else:
+        form = Complaint_form()
+        return render(request, "complaints/complaints_register.html", {"form": form})
 
 
 # TODO
