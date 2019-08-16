b04134c8f3da612db3215787edc89682d9af2b16
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-08-16 23:44:37 +0530

    Views Updated

--- complaints/urls.py
+++ complaints/urls.py
@@ -4,4 +4,5 @@ from complaints import views
 urlpatterns = [
     path("previous/", views.previous, name="previous-requests"),
     path("open-site/", views.open_site, name="open-site"),
+    path("home/", views.complaint_register, name="complaint-register"),
 ]
