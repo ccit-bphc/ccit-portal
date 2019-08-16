ae19c193c275616078f0c60fab936f1b92300989
Kushagra Gupta <kushgpt08@gmail.com>
2019-08-16 21:53:34 +0530

    installed some stuff and did some things

--- CCIT_Portal/settings/base.py
+++ CCIT_Portal/settings/base.py
@@ -30,6 +30,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "users",
+    "complaints",
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -107,3 +115,13 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
