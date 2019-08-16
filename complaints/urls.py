52b03b2de31bc7bf73c268a51ab42d2c60b95a15
Kushagra Gupta <kushgpt08@gmail.com>
2019-08-16 23:25:40 +0530

    Made some views

---
+++ complaints/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from complaints import views
+
+urlpatterns = [
+    path("previous/", views.previous, name="previous-requests"),
+    path("open-site/", views.open_site, name="open-site"),
+]
