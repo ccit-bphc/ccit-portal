acd00159473f92bdeb855e1f04d3d5fbb25b3ca9
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-08-17 17:50:30 +0530

    Model Updated

--- complaints/forms.py
+++ complaints/forms.py
@@ -4,8 +4,24 @@ from . import models
 
 class Complaint_form(forms.ModelForm):
     class Meta:
-        model=models.Complaint
-        fields = ("category", "remark", "urgency", "urgency_reason")
+        model = models.Complaint
+        fields = (
+            "category",
+            "remark",
+            "urgency",
+            "urgency_reason",
+            "contact_no",
+            "room_no",
+            "avail_start_time",
+            "avail_end_time",
+        )
+        labels = {
+            "remark": ("Complaint Details"),
+            "contact_no": ("Contact Number"),
+            "room_no": ("Room Number"),
+            #"avail_start_time": ("Complaint Details"),
+        }
+        widgets = {"contact_no": forms.Textarea(attrs={"cols": 20, "rows": 1})}
 
 
 class OpenSite_form(forms.ModelForm):
