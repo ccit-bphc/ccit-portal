3881c0e8cd9bd1c8787ad7bc7b424190c233a75b
Kushagra Gupta <kushgpt08@gmail.com>
2019-08-18 19:10:46 +0530

    Add requirement decorators on views

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from users.permissions import user_is_logged_in_and_active
 
 
+@user_is_logged_in_and_active
 def home(request):
     return render(
         request, "registration/home.html", context={"title": "CCIT Complaint Portal"}
@@ -11,3 +13,7 @@ def contact(request):
     return render(
         request, "registration/contact.html", context={"title": "CCIT Contact Us"}
     )
+
+
+def denied(request):
+    return render(request, "registration/denied.html")
