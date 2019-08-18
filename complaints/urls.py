844edeb037ee52f4b8ad8b8582f2c8541ad48426
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-08-18 11:04:55 +0530

    Formatting fixed,handle view and forms created
    HandleForm changed to ComplaintHandleForm ,User made local variable in models

--- complaints/urls.py
+++ complaints/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+"""Module for setting url paths"""
+from django.urls import path
 from complaints import views
 
 urlpatterns = [
     path("previous_tickets/", views.previous, name="previous-requests"),
     path("open-site/", views.open_site, name="open-site"),
-    path("add_tickets/", views.complaint_register, name="complaint-register"),
+    path("add_tickets/", views.register_complaint, name="complaint-register"),
+    path("handle_tickets/", views.handle_complaint, name="complaint-handle"),
 ]
--- complaints/views.py
+++ complaints/views.py
@@ -1,13 +1,12 @@
+"""Module for creating views to manage user complaints"""
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
 from django.contrib import messages
 from .models import Complaint
-from .forms import Complaint_form
-
-User = get_user_model()
+from .forms import ComplaintForm, ComplaintHandleForm
 
 
 def previous(request):
+    """View for displaying previous complaints of the user"""
     if not request.user.is_authenticated:
         return render(request, "registration/home.html", context={"title": "home"})
     user = request.user
@@ -17,11 +16,11 @@ def previous(request):
     )
 
 
-def complaint_register(request):
+def register_complaint(request):
     if not request.user.is_authenticated:
-         return render(request, "registration/home.html", context={"title": "home"})
+        return render(request, "registration/home.html", context={"title": "home"})
 
-    form = Complaint_form(request.POST)
+    form = ComplaintForm(request.POST)
     if form.is_valid():
         form_obj = form.save(commit=False)
         form_obj.user = request.user
@@ -29,11 +28,34 @@ def complaint_register(request):
         messages.success(request, "Your Complaint has been Successfully Registered")
         return render(request, "registration/home.html", context={"title": "home"})
     else:
-        form = Complaint_form()
+        form = ComplaintForm()
         return render(request, "complaints/complaints_register.html", {"form": form})
 
 
-# TODO
+def handle_complaint(request):
+    if not request.user.is_authenticated:
+        return render(request, "registration/home.html", context={"title": "home"})
+
+    form = ComplaintHandleForm(request.POST)
+    if form.is_valid():
+        form_obj = form.save(commit=False)
+        form_obj.handler = request.user
+        form_obj.save()
+        if request.POST.get("status") == "DN":
+            messages.success(request, "The Complaint has been Successfully Resolved")
+        if request.POST.get("status") == "CD":
+            messages.success(request, "The Complaint has been Successfully Cancelled")
+        return render(request, "registration/home.html", context={"title": "home"})
+    else:
+        complaints = Complaint.object.filter(status="RD").order_by("-uploaded_at")
+        return render(
+            request,
+            "complaints/handle_requests.html",
+            context={"complaints": complaints},
+        )
+
+
 def open_site(request):
+    """View For Handling Request for unblocking websites"""
     if request.method == "POST":
         pass
