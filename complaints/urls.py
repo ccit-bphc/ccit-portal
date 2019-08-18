9d21dd6236dcd6c019230b1f002a583338277b18
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-08-18 11:04:55 +0530

    Add handle_tickets path to urls.py

--- complaints/urls.py
+++ complaints/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("previous_tickets/", views.previous, name="previous-requests"),
     path("open-site/", views.open_site, name="open-site"),
     path("add_tickets/", views.complaint_register, name="complaint-register"),
+    path("handle_tickets/", views.complaint_handle, name="complaint-handle"),
 ]
