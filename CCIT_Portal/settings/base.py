a0bc1c101cbd03720cc564219bce84d6b8b3a973
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-08-19 15:50:59 +0530

    Add support for sending email to user and staff
    Format strings used for message
    
    f strings used
    
    Use Brackets with strings in messages
    
    Use f-strings in subject
    
    Use f-string in subject
    
    remove whitespace

--- CCIT_Portal/settings/base.py
+++ CCIT_Portal/settings/base.py
@@ -148,3 +148,9 @@ SOCIALACCOUNT_QUERY_EMAIL = True
 SOCIALACCOUNT_PROVIDER = {
     "google": {"SCOPE": ["profile", "email"], "AUTH_PARAMS": {"access_type": "online"}}
 }
+
+# for gmail
+EMAIL_BACKEND = "dajngo.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
