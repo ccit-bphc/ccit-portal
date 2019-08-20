30b3af03908ac8a2d9a075d67f09cad652cca4c8
Kushagra Gupta <kushgpt08@gmail.com>
2019-08-20 20:15:12 +0530

    Fixed Errors in validation schemes

--- complaints/models.py
+++ complaints/models.py
@@ -1,4 +1,5 @@
 """Module for Creating Complaints Model"""
+from datetime import time
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
@@ -37,11 +38,11 @@ class Complaint(models.Model):
     category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=REGISTERED)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    resolved_at = models.DateTimeField()
-    remark = models.TextField()
-    remark_to_user = models.TextField()
+    resolved_at = models.DateTimeField(null=True, blank=True)
+    remark = models.TextField(null=True, blank=True)
+    remark_to_user = models.TextField(null=True, blank=True)
     urgency = models.BooleanField(default=False)
-    urgency_reason = models.TextField()
+    urgency_reason = models.TextField(null=True, blank=True)
     phone_number_validator = RegexValidator(
         regex=PHONE_REGEX, message="Phone number is not in the correct format."
     )
@@ -52,14 +53,18 @@ class Complaint(models.Model):
     avail_start_time = models.TimeField(null=False, blank=False)
     avail_end_time = models.TimeField(null=False, blank=False)
     handler = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, related_name="handler"
+        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="handler"
     )
 
     def __str__(self):
         return f"{self.user} - {self.category} - {self.id}"
 
     def save(self, *args, **kwargs):
-        if (self.avail_end_time - self.avail_start_time).total_seconds() < 3600:
+        if self.avail_end_time < time(
+            self.avail_start_time.hour + 1,
+            self.avail_start_time.minute,
+            self.avail_start_time.second,
+        ):
             raise ValidationError("Available time is less than one hour.")
         if self.urgency:
             if self.urgency_reason == "":
@@ -70,10 +75,10 @@ class Complaint(models.Model):
                     "User cancelled request but status is not set to cancelled."
                 )
         if not (
-            self.handler.is_staff or self.handler == self.user or self.handler is None
+            self.handler is None or self.handler.is_staff or self.handler == self.user
         ):
             raise ValidationError("Invalid complaint handler.")
-        if self.handler.is_staff:
+        if self.handler and self.handler.is_staff:
             if self.status == self.REGISTERED:
                 raise ValidationError("Camplaint handled but status is not updated.")
         super().save(*args, **kwargs)
@@ -100,9 +105,9 @@ class UnblockRequest(models.Model):
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=REGISTERED)
     request_time = models.DateTimeField(auto_now_add=True)
     reason = models.TextField()
-    remark_to_user = models.TextField()
+    remark_to_user = models.TextField(blank=True, null=True)
     handler = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, related_name="resolver"
+        User, on_delete=models.SET_NULL, null=True, related_name="resolver", blank=True
     )
 
     def __str__(self):
@@ -114,11 +119,11 @@ class UnblockRequest(models.Model):
                 raise ValidationError(
                     "User cancelled request but status is not set to cancelled."
                 )
-        if self.handler.is_staff:
+        if self.handler and self.handler.is_staff:
             if self.status == self.REGISTERED:
                 raise ValidationError("Request handled but status not updated.")
         if not (
-            self.handler.is_staff or self.handler == self.user or self.handler is None
+            self.handler is None or self.handler.is_staff or self.handler == self.user
         ):
             raise ValidationError("Invalid request handler.")
         if not self.reason:
