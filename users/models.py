f3b39f1734c426defb80440ddc3b08cf3be52d92
Kushagra Gupta <kushgpt08@gmail.com>
2019-08-30 14:19:37 +0530

    add contact no to user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 
 class CustomUser(AbstractUser):
-    pass
+    PHONE_REGEX = r"^(\+?91[\-\s]?)?[0]?(91)?[789]\d{9}$"
+    phone_number_validator = RegexValidator(
+        regex=PHONE_REGEX, message="Phone number is not in the correct format."
+    )
+    contact_no = models.CharField(
+        max_length=15, validators=[phone_number_validator], null=True, blank=True
+    )
