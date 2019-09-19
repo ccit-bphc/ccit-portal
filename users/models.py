61227a15501ca80787a946617a7c3651a9690b0c
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-09-19 16:23:42 +0530

    Add support for sorting urgent complaints based on user type
    Add category and year before saving user

--- users/models.py
+++ users/models.py
@@ -4,6 +4,15 @@ from django.core.validators import RegexValidator
 
 
 class CustomUser(AbstractUser):
+    CATEGORY_1 = "FD"
+    CATEGORY_2 = "HD"
+    CATEGORY_3 = "PD"
+
+    CATEGORY_CHOICES = (
+        (CATEGORY_1, "First Degree"),
+        (CATEGORY_2, "Higer Degree"),
+        (CATEGORY_3, "PhD"),
+    )
     PHONE_REGEX = r"^(\+?91[\-\s]?)?[0]?(91)?[789]\d{9}$"
     phone_number_validator = RegexValidator(
         regex=PHONE_REGEX, message="Phone number is not in the correct format."
@@ -11,6 +20,8 @@ class CustomUser(AbstractUser):
     contact_no = models.CharField(
         max_length=15, validators=[phone_number_validator], null=True, blank=True
     )
+    category=models.TextField(null=False,blank=False,choices=CATEGORY_CHOICES)
+    enrollment_year=models.PositiveIntegerField(null=False,blank=False)
 
     @property
     def is_nucleus(self):
