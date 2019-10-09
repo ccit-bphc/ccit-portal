d855aa3f30eb7314d87b727dca289bfa17d5f1ef
0xVikas <vikasrvs@gmail.com>
2019-10-09 18:21:03 +0530

    Remove console.logs

--- complaints/static/main.js
+++ complaints/static/main.js
@@ -65,16 +65,12 @@ function checkURL(abc){
 		else{document.getElementById('time_alert').style.display = "none";document.getElementById('b1').removeAttribute('disabled');}
 		var today = new Date();
 		p = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
-		console.log("Today date:" + p);
 		p2 = document.getElementById('date').value.split('-');
 		p2 = p2[1] + '/' + p2[2] + '/' + p2[0];
-		console.log(p2);
 		var timenow = new Date(p + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
 		var start_time = new Date(p2 + " " + document.getElementById('hiddentime').value);
 		var start_time_a = new Date("01/01/2007 " + document.getElementById('hiddentime').value);
 		var staff_start = new Date("01/01/2007 " + "09:00:00");
-		console.log("now: " + timenow);
-		console.log("entered: " + start_time);
 		if(start_time <= timenow || start_time_a < staff_start){document.getElementById('time_alert_x').style.display = "";document.getElementById('b1').setAttribute('disabled','');}
 		else{document.getElementById('time_alert_x').style.display = "none";document.getElementById('b1').removeAttribute('disabled');}
 	};
@@ -94,14 +90,10 @@ function checkURL(abc){
 		p = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
 		p2 = document.getElementById('date').value.split('-');
 		p2 = p2[1] + '/' + p2[2] + '/' + p2[0];
-		console.log(p2);
-		console.log("Today date:" + p);
 		var timenow = new Date(p + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
 		var end_time = new Date(p2 + " " + document.getElementById('hiddentime2').value);
 		var end_time_a = new Date("01/01/2007 " + document.getElementById('hiddentime').value);
 		var staff_end = new Date("01/01/2007 " + "17:00:00");
-		console.log("now: " + timenow);
-		console.log("entered endtime: " + end_time);
 	if(end_time <= timenow || end_time_a > staff_end){document.getElementById('time_alert_x').style.display = "";document.getElementById('b1').setAttribute('disabled','');}
 		else{document.getElementById('time_alert_x').style.display = "none";document.getElementById('b1').removeAttribute('disabled');}
 	};
@@ -147,11 +139,8 @@ $(document).ready(function () {
 	$('#bhavan_list li').on('click', function(){
 		$("#dd_button").text($(this).text());
 		let arr = ["K", "R", "S", "G", "B", "V", "GT", "VM", "M", "MM", "VB", "VG", "MR", "H8"];
-		console.log("This: ");
-		console.log($(this).attr('id'));
 		let num = parseInt($(this).attr('id'));
 		document.getElementById('bhavan').value = arr[num];
-		console.log(num);
 		if(arr[num] == "MM" || arr[num] == "M" || arr[num] == "VG"){
 			document.getElementById('g_msg').style="";
 			document.getElementById('slots').style="";
