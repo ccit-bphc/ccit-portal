f561fbf6b9344b9c6a83c532653b08533842cfa4
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-11-08 04:10:17 +0530

    Update Text in Admin Site

--- complaints/admin.py
+++ complaints/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from . import models
 
+admin.site.site_header = 'Computer Centre IT  BITS Pilani,Hyderabad'
+admin.site.site_title = 'Administration Page'
+admin.site.index_title='CCIT Portal Admin Page'
+
 admin.site.register(models.Complaint)
 admin.site.register(models.UnblockRequest)
